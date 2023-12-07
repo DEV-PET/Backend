dbb924a0701d38d79f125cc1245a6ec2c6395577
Luigi Sousa <123770497+LuigiSousaa@users.noreply.github.com>
2023-12-07 16:17:58 -0300

    Delete conecta.php

--- conecta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("HOST","localhost");
-define("USUARIO","root");
-define("SENHA","");
-define("DB","devpet");
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die(mysqli_connect_error());
-?> 
